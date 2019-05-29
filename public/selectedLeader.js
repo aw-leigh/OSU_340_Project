1994725c577837fbbdc6c5aeae43620e25451f97
aw-leigh <puddingisfun@gmail.com>
2019-05-29 14:59:36 +0900

    updated handlebars

--- public/selectedLeader.js
+++ public/selectedLeader.js
@@ -1,4 +1,6 @@
 function selectAlignment(leader){
+    console.log(leader);
+    console.log("leader");
     $("#leader-selector").value(leader);
     //Andrew: not sure why this isn't working :/
 }
