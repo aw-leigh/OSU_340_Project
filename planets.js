4a4be2f35ae34b0b5618b1da7dcaae1681687a58
albrechtmc <43507571+albrechtmc@users.noreply.github.com>
2019-05-28 19:08:33 -1000

    Add files via upload

--- planets.js
+++ planets.js
@@ -3,7 +3,7 @@ module.exports = function(){
     var router = express.Router();
 
     function servePlanets(req, res){
-        var query = `SELECT NAME FROM Planets
+        var query = `SELECT NAME, ID FROM Planets
                      ORDER BY Planets.Name`;
         var mysql = req.app.get('mysql');
         var context = {};
@@ -23,6 +23,53 @@ module.exports = function(){
         //res.send('Here you go!');
     }
 
+    function getPlanet(res, mysql, context, id, complete) {
+        var sql = `SELECT NAME, ID FROM Planets WHERE ID= ?`;
+        //date needs to be in the above format to work with the HTML date input
+        var inserts = [id];
+        mysql.pool.query(sql, inserts, function (error, results, fields) {
+            if (error) {
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.planet = results[0];
+            complete();
+        });
+    }
+
     router.get('/', servePlanets);
+
+    router.get('/:id', function (req, res) {
+        callbackCount = 0;
+        var context = {};
+        context.jsscripts = ["selectedPhase.js", "updatePlanet.js"];
+        var mysql = req.app.get('mysql');
+        getPlanet(res, mysql, context, req.params.id, complete);
+        function complete() {
+            callbackCount++;
+            if (callbackCount >= 1) {
+                res.render('planets-update', context);
+            }
+        }
+    });
+
+    router.put('/:id', function (req, res) {
+        var mysql = req.app.get('mysql');
+        console.log(req.body)
+        console.log(req.params.id)
+        var sql = "UPDATE Planets SET Name=? WHERE ID=?";
+        var inserts = [req.body.name, req.params.id];
+        sql = mysql.pool.query(sql, inserts, function (error, results, fields) {
+            if (error) {
+                console.log(error)
+                res.write(JSON.stringify(error));
+                res.end();
+            } else {
+                res.status(200);
+                res.end();
+            }
+        });
+    });
+
     return router;
 }();
