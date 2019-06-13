2d57aa3a9d069ac5e8c5fa9a47237289040e6332
Andrew Wilson <aw-leigh@users.noreply.github.com>
2019-06-13 14:07:32 +0900

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -3,7 +3,7 @@ var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
   user            : 'cs340_wilsoan6',
-  password        : '&3zGmE',
+  password        : 'redacted',
   database        : 'cs340_wilsoan6'
 });
 module.exports.pool = pool;
